b906daf860d0997de57365e162636a013d159375
zensh <admin@zensh.com>
2016-01-27 20:43:01 +0800

    use standard style

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 /*
  * BUFSP
  * https://github.com/teambition/bufsp
@@ -6,154 +6,155 @@
  * Licensed under the MIT license.
  */
 
-var util = require('util');
-var EventEmitter = require('events').EventEmitter;
-var CRLF = '\r\n';
+var util = require('util')
+var EventEmitter = require('events').EventEmitter
+var CRLF = '\r\n'
 
-module.exports = Bufsp;
-Bufsp.encode = encode;
-Bufsp.decode = decode;
+module.exports = Bufsp
+Bufsp.encode = encode
+Bufsp.decode = decode
 
-function Bufsp(options) {
-  if (!(this instanceof Bufsp)) return new Bufsp(options);
-  options = options || {};
+function Bufsp (options) {
+  if (!(this instanceof Bufsp)) return new Bufsp(options)
+  options = options || {}
 
-  this._encoding = options.encoding || 'utf8';
-  this._stringEncoding = !!options.returnString && this._encoding;
+  this._encoding = options.encoding || 'utf8'
+  this._stringEncoding = !!options.returnString && this._encoding
 
   // legacy from old stream.
-  this.writable = true;
+  this.writable = true
 
-  clearState(this);
-  EventEmitter.call(this);
+  clearState(this)
+  EventEmitter.call(this)
 }
-util.inherits(Bufsp, EventEmitter);
+util.inherits(Bufsp, EventEmitter)
 
-Bufsp.prototype.encode = function(val, encoding) {
-  return encode(val, encoding || this._encoding);
-};
+Bufsp.prototype.encode = function (val, encoding) {
+  return encode(val, encoding || this._encoding)
+}
 
-Bufsp.prototype.decode = function(val, encoding) {
-  return decode(val, encoding || this._stringEncoding);
-};
+Bufsp.prototype.decode = function (val, encoding) {
+  return decode(val, encoding || this._stringEncoding)
+}
 
-Bufsp.prototype.write = function(chunk) {
+Bufsp.prototype.write = function (chunk) {
   if (!Buffer.isBuffer(chunk)) {
-    this.emit('error', new Error('Invalid buffer chunk'));
-    return true;
+    this.emit('error', new Error('Invalid buffer chunk'))
+    return true
   }
 
-  if (!this._buffer) this._buffer = chunk;
+  if (!this._buffer) this._buffer = chunk
   else {
-    var ret = this._buffer.length - this._index;
-    var concatBuffer = new Buffer(chunk.length + ret);
+    var ret = this._buffer.length - this._index
+    var concatBuffer = new Buffer(chunk.length + ret)
 
-    this._buffer.copy(concatBuffer, 0, this._index);
-    chunk.copy(concatBuffer, ret);
-    this._buffer = concatBuffer;
-    this._index = 0;
+    this._buffer.copy(concatBuffer, 0, this._index)
+    chunk.copy(concatBuffer, ret)
+    this._buffer = concatBuffer
+    this._index = 0
   }
 
   while (this._index < this._buffer.length) {
-    var result = parseBuffer(this._buffer, this._index, this._stringEncoding);
+    var result = parseBuffer(this._buffer, this._index, this._stringEncoding)
     if (result == null) {
-      this.emit('drain');
-      return true;
+      this.emit('drain')
+      return true
     }
     if (result instanceof Error) {
-      clearState(this);
-      this.emit('error', result);
-      return false;
+      clearState(this)
+      this.emit('error', result)
+      return false
     }
-    this._index = result.index;
-    this.emit('data', result.content);
+    this._index = result.index
+    this.emit('data', result.content)
   }
 
-  clearState(this).emit('drain');
-  return true;
-};
-
-Bufsp.prototype.end = function(chunk) {
-  if (chunk) this.write(chunk);
-  this.emit('finish');
-};
-
-function clearState(ctx) {
-  ctx._index = 0;
-  ctx._buffer = null;
-  return ctx;
+  clearState(this).emit('drain')
+  return true
 }
 
-function encode(val, encoding) {
-  if (val == null) return new Buffer('$-1\r\n');
+Bufsp.prototype.end = function (chunk) {
+  if (chunk) this.write(chunk)
+  this.emit('finish')
+}
+
+function clearState (ctx) {
+  ctx._index = 0
+  ctx._buffer = null
+  return ctx
+}
+
+function encode (val, encoding) {
+  if (val == null) return new Buffer('$-1\r\n')
 
   if (util.isError(val)) {
-    val = '-' + val.name + ' ' + val.message + CRLF;
-    return new Buffer(val);
+    val = '-' + val.name + ' ' + val.message + CRLF
+    return new Buffer(val)
   }
 
   if (!Buffer.isBuffer(val)) {
-    if (typeof val !== 'string') throw new Error('Invalid value to encode');
-    val = new Buffer(val, encoding);
+    if (typeof val !== 'string') throw new Error('Invalid value to encode')
+    val = new Buffer(val, encoding)
   }
-  var str = '$' + val.length + CRLF;
-  var buffer = new Buffer(str.length + val.length + 2);
-  buffer.write(str);
-  val.copy(buffer, str.length);
-  buffer.write(CRLF, str.length + val.length);
-  return buffer;
+  var str = '$' + val.length + CRLF
+  var buffer = new Buffer(str.length + val.length + 2)
+  buffer.write(str)
+  val.copy(buffer, str.length)
+  buffer.write(CRLF, str.length + val.length)
+  return buffer
 }
 
-function decode(buffer, encoding) {
-  if (!Buffer.isBuffer(buffer)) throw new Error('Invalid buffer chunk');
-  var result = parseBuffer(buffer, 0, encoding);
-  if (!result || result.index < buffer.length) throw new Error('Decode failed');
-  if (result instanceof Error) throw result;
-  return result.content;
+function decode (buffer, encoding) {
+  if (!Buffer.isBuffer(buffer)) throw new Error('Invalid buffer chunk')
+  var result = parseBuffer(buffer, 0, encoding)
+  if (!result || result.index < buffer.length) throw new Error('Decode failed')
+  if (result instanceof Error) throw result
+  return result.content
 }
 
-function readBuffer(buffer, index) {
-  var start = index;
-  while (index < buffer.length && !isCRLF(buffer, index)) index++;
+function readBuffer (buffer, index) {
+  var start = index
+  while (index < buffer.length && !isCRLF(buffer, index)) index++
 
   return index >= buffer.length ? null : {
     content: buffer.utf8Slice(start, index),
     index: index + 2
-  };
+  }
 }
 
-function parseBuffer(buffer, index, stringEncoding) {
-  var len = NaN, result = null;
-  if (index >= buffer.length) return null;
+function parseBuffer (buffer, index, stringEncoding) {
+  var len = NaN
+  var result = null
+  if (index >= buffer.length) return null
 
   switch (buffer[index]) {
     case 45: // '-'
-      result = readBuffer(buffer, index + 1);
-      if (result == null) return result;
-      var fragment = result.content.match(/^(\S+) ([\s\S]+)$/);
-      if (!fragment) return new Error('Parse "-" failed');
-      result.content = new Error(fragment[2]);
-      result.content.name = fragment[1];
-      return result;
+      result = readBuffer(buffer, index + 1)
+      if (result == null) return result
+      var fragment = result.content.match(/^(\S+) ([\s\S]+)$/)
+      if (!fragment) return new Error('Parse "-" failed')
+      result.content = new Error(fragment[2])
+      result.content.name = fragment[1]
+      return result
 
     case 36: // '$'
-      result = readBuffer(buffer, index + 1);
-      if (result == null) return result;
-      len = +result.content;
-      if (!result.content.length || len !== len) return new Error('Parse "$" failed, invalid length');
-      if (len === -1) result.content = null;
-      else if (buffer.length < result.index + len + 2) return null;
-      else if (!isCRLF(buffer, result.index + len)) return new Error('Parse "$" failed, invalid CRLF');
+      result = readBuffer(buffer, index + 1)
+      if (result == null) return result
+      len = +result.content
+      if (!result.content.length || Number.isNaN(len)) return new Error('Parse "$" failed, invalid length')
+      if (len === -1) result.content = null
+      else if (buffer.length < result.index + len + 2) return null
+      else if (!isCRLF(buffer, result.index + len)) return new Error('Parse "$" failed, invalid CRLF')
       else {
-        result.content = buffer.slice(result.index, result.index + len);
-        if (stringEncoding) result.content = result.content.toString(stringEncoding);
-        result.index = result.index + len + 2;
+        result.content = buffer.slice(result.index, result.index + len)
+        if (stringEncoding) result.content = result.content.toString(stringEncoding)
+        result.index = result.index + len + 2
       }
-      return result;
+      return result
   }
-  return new Error('Invalid Buffer: parse failed');
+  return new Error('Invalid Buffer: parse failed')
 }
 
-function isCRLF(buffer, index) {
-  return buffer[index] === 13 && buffer[index + 1] === 10;
+function isCRLF (buffer, index) {
+  return buffer[index] === 13 && buffer[index + 1] === 10
 }
--- test/index.js
+++ test/index.js
@@ -1,220 +1,218 @@
-'use strict';
-/*global describe, it, before, after, beforeEach, afterEach*/
+'use strict'
+/*global describe, it, after*/
 
-var fs = require('fs');
-var net = require('net');
-var assert = require('assert');
-var Bufsp = require('../index.js');
+var fs = require('fs')
+var net = require('net')
+var assert = require('assert')
+var Bufsp = require('../index.js')
 
-var echoServer = net.createServer(function(socket) {
-  socket.on('data', function(chunk) {
-    socket.write(chunk);
-  });
-});
-echoServer.listen(2999);
+var echoServer = net.createServer(function (socket) {
+  socket.on('data', function (chunk) {
+    socket.write(chunk)
+  })
+})
+echoServer.listen(2999)
 
-describe('Bufsp', function() {
-  after(function(callback) {
-    setTimeout(function() {
-      process.exit(0);
-      callback();
-    }, 1000);
-  });
+describe('Bufsp', function () {
+  after(function (callback) {
+    setTimeout(function () {
+      process.exit(0)
+      callback()
+    }, 1000)
+  })
 
-  it('Bufsp.encode', function(done) {
-    assert(Bufsp.encode(null).equals(new Buffer('$-1\r\n')));
-    assert(Bufsp.encode(new Error('error')).equals(new Buffer('-Error error\r\n')));
-    assert(Bufsp.encode(new TypeError('error')).equals(new Buffer('-TypeError error\r\n')));
-    assert(Bufsp.encode('').equals(new Buffer('$0\r\n\r\n')));
-    assert(Bufsp.encode('abcdefg').equals(new Buffer('$7\r\nabcdefg\r\n')));
-    assert(Bufsp.encode('中文汉字没问题').equals(new Buffer('$21\r\n中文汉字没问题\r\n')));
-    assert(Bufsp.encode(new Buffer(10).fill(0)).equals(new Buffer('$10\r\n\0\0\0\0\0\0\0\0\0\0\r\n')));
-    assert(Bufsp.encode('QlVGU1A=', 'base64').equals(new Buffer('$5\r\nBUFSP\r\n')));
-    assert(Bufsp.encode('4255465350', 'hex').equals(new Buffer('$5\r\nBUFSP\r\n')));
+  it('Bufsp.encode', function (done) {
+    assert(Bufsp.encode(null).equals(new Buffer('$-1\r\n')))
+    assert(Bufsp.encode(new Error('error')).equals(new Buffer('-Error error\r\n')))
+    assert(Bufsp.encode(new TypeError('error')).equals(new Buffer('-TypeError error\r\n')))
+    assert(Bufsp.encode('').equals(new Buffer('$0\r\n\r\n')))
+    assert(Bufsp.encode('abcdefg').equals(new Buffer('$7\r\nabcdefg\r\n')))
+    assert(Bufsp.encode('中文汉字没问题').equals(new Buffer('$21\r\n中文汉字没问题\r\n')))
+    assert(Bufsp.encode(new Buffer(10).fill(0)).equals(new Buffer('$10\r\n\0\0\0\0\0\0\0\0\0\0\r\n')))
+    assert(Bufsp.encode('QlVGU1A=', 'base64').equals(new Buffer('$5\r\nBUFSP\r\n')))
+    assert(Bufsp.encode('4255465350', 'hex').equals(new Buffer('$5\r\nBUFSP\r\n')))
 
-    assert.throws(function() { Bufsp.encode(0); });
-    assert.throws(function() { Bufsp.encode(NaN); });
-    assert.throws(function() { Bufsp.encode({}); });
-    assert.throws(function() { Bufsp.encode([]); });
+    assert.throws(function () { Bufsp.encode(0) })
+    assert.throws(function () { Bufsp.encode(NaN) })
+    assert.throws(function () { Bufsp.encode({}) })
+    assert.throws(function () { Bufsp.encode([]) })
 
-    done();
-  });
+    done()
+  })
 
-  it('Bufsp.decode', function(done) {
-    assert(Bufsp.decode(Bufsp.encode(null)) === null);
-    assert(Bufsp.decode(Bufsp.encode(null), 'utf8') === null);
+  it('Bufsp.decode', function (done) {
+    assert(Bufsp.decode(Bufsp.encode(null)) === null)
+    assert(Bufsp.decode(Bufsp.encode(null), 'utf8') === null)
 
-    assert(Bufsp.decode(Bufsp.encode('null')).equals(new Buffer('null')));
-    assert(Bufsp.decode(Bufsp.encode('null'), 'utf8') === 'null');
+    assert(Bufsp.decode(Bufsp.encode('null')).equals(new Buffer('null')))
+    assert(Bufsp.decode(Bufsp.encode('null'), 'utf8') === 'null')
 
-    var error = Bufsp.decode(Bufsp.encode(new Error('test')));
-    assert(error.name === 'Error');
-    assert(error.message === 'test');
+    var error = Bufsp.decode(Bufsp.encode(new Error('test')))
+    assert(error.name === 'Error')
+    assert(error.message === 'test')
 
-    error = Bufsp.decode(Bufsp.encode(new TypeError('test')));
-    assert(error.name === 'TypeError');
-    assert(error.message === 'test');
+    error = Bufsp.decode(Bufsp.encode(new TypeError('test')))
+    assert(error.name === 'TypeError')
+    assert(error.message === 'test')
 
-    assert(Bufsp.decode(Bufsp.encode(new Buffer(10).fill(9))).equals(new Buffer(10).fill(9)));
-    assert.throws(function() { Bufsp.decode(Bufsp.encode(new Error('test')).slice(1)); });
-    assert.throws(function() { Bufsp.decode(Bufsp.encode('null').slice(0, -1)); });
+    assert(Bufsp.decode(Bufsp.encode(new Buffer(10).fill(9))).equals(new Buffer(10).fill(9)))
+    assert.throws(function () { Bufsp.decode(Bufsp.encode(new Error('test')).slice(1)) })
+    assert.throws(function () { Bufsp.decode(Bufsp.encode('null').slice(0, -1)) })
 
-    done();
-  });
+    done()
+  })
 
-  it('new Bufsp()', function(done) {
-    var bufsp = new Bufsp();
-    var socket = net.createConnection(2999);
+  it('new Bufsp()', function (done) {
+    var bufsp = new Bufsp()
+    var socket = net.createConnection(2999)
     var tasks = [
       null,
       new Error('test'),
       '12345',
       JSON.stringify([1, 2, 3, 4, 5])
-    ];
-    var res = [];
+    ]
+    var res = []
 
-    var calledDrain = null;
+    var calledDrain = null
     socket.pipe(bufsp)
       .on('error', done)
-      .on('data', function(message) {
-        res.push(message);
+      .on('data', function (message) {
+        res.push(message)
       })
-      .on('drain', function() {
-        calledDrain = true;
+      .on('drain', function () {
+        calledDrain = true
       })
-      .on('finish', function() {
-        assert(calledDrain === true);
-        assert(res[0] === tasks[0]);
-        assert(res[1] instanceof Error);
-        assert(res[2] instanceof Buffer);
-        assert(res[2].toString() === tasks[2]);
-        assert(res[3].toString() === tasks[3]);
-        done();
-      });
+      .on('finish', function () {
+        assert(calledDrain === true)
+        assert(res[0] === tasks[0])
+        assert(res[1] instanceof Error)
+        assert(res[2] instanceof Buffer)
+        assert(res[2].toString() === tasks[2])
+        assert(res[3].toString() === tasks[3])
+        done()
+      })
 
-    tasks.map(function(data) {
-      socket.write(bufsp.encode(data));
-    });
+    tasks.map(function (data) {
+      socket.write(bufsp.encode(data))
+    })
 
-    socket.end();
-  });
+    socket.end()
+  })
 
-  it('new Bufsp({returnString: true})', function(done) {
-    var bufsp = new Bufsp({returnString: true});
-    var socket = net.createConnection(2999);
+  it('new Bufsp({returnString: true})', function (done) {
+    var bufsp = new Bufsp({returnString: true})
+    var socket = net.createConnection(2999)
     var tasks = [
       null,
       new Error('test'),
       '12345',
       JSON.stringify([1, 2, 3, 4, 5])
-    ];
-    var res = [];
+    ]
+    var res = []
 
-    var calledDrain = null;
+    var calledDrain = null
     socket.pipe(bufsp)
       .on('error', done)
-      .on('data', function(message) {
-        res.push(message);
+      .on('data', function (message) {
+        res.push(message)
       })
-      .on('drain', function() {
-        calledDrain = true;
+      .on('drain', function () {
+        calledDrain = true
       })
-      .on('finish', function() {
-        assert(calledDrain === true);
-        assert(res[0] === tasks[0]);
-        assert(res[1] instanceof Error);
-        assert(res[2] === tasks[2]);
-        assert(res[3] === tasks[3]);
-        done();
-      });
+      .on('finish', function () {
+        assert(calledDrain === true)
+        assert(res[0] === tasks[0])
+        assert(res[1] instanceof Error)
+        assert(res[2] === tasks[2])
+        assert(res[3] === tasks[3])
+        done()
+      })
 
-    tasks.map(function(data) {
-      socket.write(bufsp.encode(data));
-    });
+    tasks.map(function (data) {
+      socket.write(bufsp.encode(data))
+    })
 
-    socket.end();
-  });
+    socket.end()
+  })
 
-  it('new Bufsp() with error data', function(done) {
-    var bufsp = new Bufsp({returnString: true});
-    var socket = net.createConnection(2999);
-    var res = [];
+  it('new Bufsp() with error data', function (done) {
+    var bufsp = new Bufsp({returnString: true})
+    var socket = net.createConnection(2999)
+    var res = []
 
-    var calledDrain = null;
     socket.pipe(bufsp)
-      .on('error', function(err) {
-        assert(err.message === 'Invalid Buffer: parse failed');
-        assert(err instanceof Error);
-        done();
+      .on('error', function (err) {
+        assert(err.message === 'Invalid Buffer: parse failed')
+        assert(err instanceof Error)
+        done()
       })
-      .on('data', function(message) {
-        res.push(message);
+      .on('data', function (message) {
+        res.push(message)
       })
-      .on('drain', function() {
-        calledDrain = true;
-      });
+      .on('drain', function () {})
 
-    socket.write(new Buffer('not bufsp chunk'));
-    socket.end();
-  });
+    socket.write(new Buffer('not bufsp chunk'))
+    socket.end()
+  })
 
-  it('new Bufsp() with error data', function(done) {
-    var bufsp = new Bufsp({returnString: true});
-    var socket = net.createConnection(2999);
-    var res = [];
+  it('new Bufsp() with error data', function (done) {
+    var bufsp = new Bufsp({returnString: true})
+    var socket = net.createConnection(2999)
+    var res = []
 
-    var calledDrain = null;
+    var calledDrain = null
     socket.pipe(bufsp)
-      .on('error', function(err) {
-        assert(calledDrain === null);
-        assert(res[0] === null);
-        assert(res.length === 1);
-        done();
+      .on('error', function (err) {
+        assert(err instanceof Error)
+        assert(calledDrain === null)
+        assert(res[0] === null)
+        assert(res.length === 1)
+        done()
       })
-      .on('data', function(message) {
-        res.push(message);
+      .on('data', function (message) {
+        res.push(message)
       })
-      .on('drain', function() {
-        calledDrain = true;
-      });
+      .on('drain', function () {
+        calledDrain = true
+      })
 
-    socket.write(bufsp.encode(null));
-    socket.write(new Buffer('\r\n\r\n'));
-    socket.write(bufsp.encode('12345'));
-    socket.write(bufsp.encode(new Buffer('$2\r\nabc\r\n')));
+    socket.write(bufsp.encode(null))
+    socket.write(new Buffer('\r\n\r\n'))
+    socket.write(bufsp.encode('12345'))
+    socket.write(bufsp.encode(new Buffer('$2\r\nabc\r\n')))
 
-    socket.end();
-  });
+    socket.end()
+  })
 
-  it('new Bufsp() Pipelining data', function(done) {
-    var bufsp = new Bufsp({returnString: true});
-    var socket = net.createConnection(2999);
-    var tasks = [fs.readFileSync('index.js')];
-    var res = [];
-    for (var i = 0; i < 100000; i++) tasks.push(i + '');
-    var concatBuffer = Buffer.concat(tasks.map(function(data) {
-      return bufsp.encode(data);
-    }));
+  it('new Bufsp() Pipelining data', function (done) {
+    var bufsp = new Bufsp({returnString: true})
+    var socket = net.createConnection(2999)
+    var tasks = [fs.readFileSync('index.js')]
+    var res = []
+    for (var i = 0; i < 100000; i++) tasks.push(i + '')
+    var concatBuffer = Buffer.concat(tasks.map(function (data) {
+      return bufsp.encode(data)
+    }))
 
-    var count = 0;
+    var count = 0
 
-    socket.on('data', function() {
-      count++;
-    });
+    socket.on('data', function () {
+      count++
+    })
 
     socket.pipe(bufsp)
       .on('error', done)
-      .on('data', function(message) {
-        res.push(message);
+      .on('data', function (message) {
+        res.push(message)
       })
-      .on('finish', function() {
-        assert(count > 2);
-        res.forEach(function(data, index) {
-          assert(tasks[index].toString() === data);
-        });
-        done();
-      });
+      .on('finish', function () {
+        assert(count > 2)
+        res.forEach(function (data, index) {
+          assert(tasks[index].toString() === data)
+        })
+        done()
+      })
 
-    socket.end(concatBuffer);
-  });
-});
+    socket.end(concatBuffer)
+  })
+})
